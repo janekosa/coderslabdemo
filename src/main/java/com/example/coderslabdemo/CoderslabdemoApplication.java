e04022ae035e6a0d9fd2020e9167716bd3cb0357
janekosa <janekossowski@gmail.com>
2019-09-17 21:49:30 +0200

    initial project structure

---
+++ src/main/java/com/example/coderslabdemo/CoderslabdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderslabdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderslabdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderslabdemoApplication.class, args);
+	}
+
+}
